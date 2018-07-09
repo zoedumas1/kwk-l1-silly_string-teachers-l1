4b73ee7b94b904928b83f1e98bf6730637ba6e0c
Zoe Dumas <zoe.dumas1@yahoo.com>
2018-07-09 19:59:03 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -23,7 +23,7 @@ puts lowercased_phrase
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = big_word.length # Put a . after big_word and use a String method.
 
 puts "There are #{letter_count} in #{big_word}"
 
